e10e81e38eada0ffd6b6a98ca231ad769b7722a5
ParvinMusayev <parvinmusayev1@gmail.com>
2023-11-29 18:31:09 +0400

    created profile page

---
+++ src/app/profile/page.js
@@ -0,0 +1,14 @@
+"use client"
+import { useSession } from "next-auth/react"
+
+export default function ProfilePage() {
+    const session = useSession()
+    console.log(session)
+    return (
+        <section className="mt-8">
+            <h1 className="text-center text-primary text-4xl mb-4">
+                Profile
+            </h1>
+        </section>
+    )
+}
