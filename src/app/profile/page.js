2883655cd48a00a5c004b0ca29538d39d6dddb6b
ParvinMusayev <parvinmusayev1@gmail.com>
2023-11-29 18:45:36 +0400

    uptaded next.config.js

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,14 +1,38 @@
 "use client"
 import { useSession } from "next-auth/react"
+import Image from "next/image"
+import { redirect } from "next/navigation"
 
 export default function ProfilePage() {
     const session = useSession()
+    const {status} = session
     console.log(session)
+    
+
+    if(status === "loading"){
+        return "Loading..."
+    }
+
+    if (status === 'unauthenticated') {
+        return redirect('/login')
+    }
+
+    const userImage = session.data?.user?.image
+
     return (
         <section className="mt-8">
             <h1 className="text-center text-primary text-4xl mb-4">
                 Profile
             </h1>
+
+            <form className="max-w-xs mx-auto border">
+                <div>
+                    <div className="">
+                        <Image src={userImage} alt={"avatar"} width={64} height={64} />
+                    </div>
+                </div>
+
+            </form>
         </section>
     )
 }
