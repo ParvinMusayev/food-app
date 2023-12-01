58e82c3935a56d5a5f74fb2bbf47822f3dce9544
ParvinMusayev <parvinmusayev1@gmail.com>
2023-12-01 21:28:09 +0400

    Paused project development

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -43,7 +43,6 @@ export default function ProfilePage() {
     await  fetch('/api/upload', {
         method: "POST",
         body: data,
-        headers:{"Content-Type": "multipart/form-data"}
       })
     }
   }
