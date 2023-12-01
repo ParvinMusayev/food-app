af6347d02aa7fc710c330eb1ed6f00c8a5cc47ec
ParvinMusayev <parvinmusayev1@gmail.com>
2023-12-01 19:33:14 +0400

    Implement database updates for profile changes

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -6,25 +6,46 @@ import { useEffect, useState } from "react";
 
 export default function ProfilePage() {
   const session = useSession();
-  const [userName, setUserName] = useState( "");
+  const [userName, setUserName] = useState("");
+  const [saved, setSaved] = useState(false);
+  const [isSaving, setIsSaving] = useState(false);
   const { status } = session;
 
   useEffect(() => {
-    if(status === 'authenticated'){
-      setUserName(session.data.user.name)
+    if (status === "authenticated") {
+      setUserName(session.data.user.name);
     }
-  },[session, status])
+  }, [session, status]);
 
-  console.log(session)
+  console.log(session);
 
   async function handleProfileInfoUptade(ev) {
     ev.preventDefault();
-    const response= await fetch("/api/profile", {
+    setSaved(false);
+    setIsSaving(true);
+    const response = await fetch("/api/profile", {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({name: userName})
+      body: JSON.stringify({ name: userName }),
     });
+    setIsSaving(false);
+    if (response.ok) {
+      setSaved(true);
+    }
+  }
 
+  async function handleFileChange(ev) {
+    console.log("object")
+    const files = ev.target.files;
+    if (files?.length === 1) {
+      const data = new FormData
+      data.set("file", files[0])
+    await  fetch('/api/upload', {
+        method: "POST",
+        body: data,
+        headers:{"Content-Type": "multipart/form-data"}
+      })
+    }
   }
 
   if (status === "loading") {
@@ -42,6 +63,16 @@ export default function ProfilePage() {
       <h1 className="text-center text-primary text-4xl mb-4">Profile</h1>
 
       <div className="max-w-md mx-auto ">
+        {saved && (
+          <h2 className="text-center bg-green-100 p-4 rounded-lg border border-green-300">
+            Profile saved!
+          </h2>
+        )}
+        {isSaving && (
+          <h2 className="text-center bg-blue-100 p-4 rounded-lg border border-blue-300">
+            Saving...
+          </h2>
+        )}
         <div className="flex gap-4 items-center">
           <div>
             <div className=" p-2 rounded-lg relative">
@@ -53,7 +84,16 @@ export default function ProfilePage() {
                 alt={"avatar"}
               />
 
-              <button type="button">Edit</button>
+              <label>
+                <input
+                  type="file"
+                  className="hidden"
+                  onChange={handleFileChange}
+                />
+                <span className="block border border-gray-300 rounded-lg p-2 text-center cursor-pointer">
+                  Edit
+                </span>
+              </label>
             </div>
           </div>
           <form className="grow" onSubmit={handleProfileInfoUptade}>
